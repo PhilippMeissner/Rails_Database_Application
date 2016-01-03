d3077211ab248f988df285ddba6d92c0292ce04b
PhilippMeissner <pm@zeit.io>
2016-01-03 14:58:42 +0100

    added two more routes for possible charts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'get_data' => 'static#get_data', defaults: {format: :json}
+  get 'get_data_second' => 'static#get_data_second', defaults: {format: :json}
+  get 'get_data_third' => 'static#get_data_third', defaults: {format: :json}
   root 'static#home'
 end
